ff23dc365e1501533df54a717734d4e17769b741
daxapps <daxapps777@gmail.com>
2017-04-21 17:42:55 -0500

    Improve UI

--- app.js
+++ app.js
@@ -179,6 +179,12 @@ var UIController = (function() {
 
 	};
 
+	var nodeListForEach = function(list, callback) {
+		for (var i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
+	};
+
 	return {
 		getInput: function() {
 			return {
@@ -252,12 +258,6 @@ var UIController = (function() {
 
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			var nodeListForEach = function(list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
-
 			nodeListForEach(fields, function(current, index) {
 
 				if (percentages[index] > 0) {
@@ -278,6 +278,20 @@ var UIController = (function() {
 
 			year = now.getFullYear();
 			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
+
+		changedType: function() {
+
+			var fields = document.querySelectorAll(
+				DOMstrings.inputType + ',' +
+				DOMstrings.inputDescription + ',' +
+				DOMstrings.inputValue);
+
+			nodeListForEach(fields, function(cur) {
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 		},
 
 		getDOMstrings: function() {
@@ -304,6 +318,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 	};
 
 	var updateBudget = function() {
