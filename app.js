bd26e0bd21911c243be9936318ff16f3059351af
daxapps <daxapps777@gmail.com>
2017-04-20 21:24:09 -0500

    Create initialization function

--- app.js
+++ app.js
@@ -34,13 +34,24 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
 
 	var ctrlAddItem = function() {
 
 		// 1. Get the field input data
 		var input = UICtrl.getInput();
-		console.log(input);
 
 		// 2. Add the item to the budget controller
 
@@ -50,21 +61,18 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 		// 5. Display the budget on the UI
 
-		console.log("It works!");
+	};
 
-	}
-
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			console.log('Application has started.');
+			setupEventListeners();
 		}
-
-	});
+	};
 
 })(budgetController, UIController);
+
+controller.init();
 
 
 
