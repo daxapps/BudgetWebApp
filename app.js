e81e62126404abe168e283ceef37084b341305be
daxapps <daxapps777@gmail.com>
2017-04-21 13:05:24 -0500

    Enable item deletion

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ var budgetController = (function() {
 			return newItem;
 		},
 
+		deleteItem: function(type, id) {
+			var ids, index;
+
+			ids = data.allItems[type].map(function(current) {
+				return current.id;
+			});
+
+			index = ids.indexOf(id);
+
+			if (index !== -1) {
+				data.allItems[type].splice(index, 1);
+			}
+		},
+
 		calculateBudget: function() {
 
 			// calculate total income and expenses
@@ -142,6 +156,13 @@ var UIController = (function() {
 
 			// Insert the HTML into the DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+		},
+
+		deleteListItem: function(selectorID) {
+
+			var el = document.getElementById(selectorID);
+			el.parentNode.removeChild(el);
 
 		},
 
@@ -244,13 +265,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 			// inc-1
 			splitID = itemID.split('-');
 			type = splitID[0];
-			ID = splitID[1];
+			ID = parseInt(splitID[1]);
 
 			// 1. Delete the item from the data structure
+			budgetCtrl.deleteItem(type, ID);
 
 			// 2. Delete the item from the UI
+			UICtrl.deleteListItem(itemID);
 
 			// 3. Update and show the new budget
+			updateBudget();
 
 		}
 	};
